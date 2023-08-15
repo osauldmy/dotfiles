a9f8332691a6e5b2a87628c1ac3dce2f21ed8814
Dmytro Osaulenko <osauldmy@gmail.com>
2023-08-15 15:33:25 +0200

    nvim: project: fix wrong setup of project.nvim

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,22 +1,24 @@
 return {
   "ahmedkhalf/project.nvim",
   event = "VeryLazy",
-  opts = {
-    patterns = {
-      ".git",
-      "Makefile",
-      "package.json",
-      "pyproject.toml",
-      "setup.py",
-      "Dockerfile",
-      "docker-compose.yml",
-      "docker-compose.yaml",
-    },
-    silent_chdir = true,
-    detection_methods = { "lsp", "patterns" },
-    manual_mode = false,
-  },
+  -- lazy = false,
   config = function()
+    require("project_nvim").setup({
+      silent_chdir = true,
+      manual_mode = false,
+      detection_methods = { "lsp", "patterns" },
+      patterns = {
+        ".git",
+        "Makefile",
+        "package.json",
+        "pyproject.toml",
+        "setup.py",
+        "Dockerfile",
+        "docker-compose.yml",
+        "docker-compose.yaml",
+      },
+    })
+
     require("telescope").load_extension("projects")
   end,
 }
