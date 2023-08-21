20b9f4a2af3d45ebf0cde15284f175214e005963
Dmytro Osaulenko <osauldmy@gmail.com>
2023-08-21 12:26:51 +0200

    nvim: use naive way to open Telescope projects when no file is specified

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,7 +1,6 @@
 return {
   "ahmedkhalf/project.nvim",
   event = "VeryLazy",
-  -- lazy = false,
   config = function()
     require("project_nvim").setup({
       silent_chdir = true,
@@ -19,6 +18,16 @@ return {
       },
     })
 
-    require("telescope").load_extension("projects")
+    local telescope = require("telescope")
+    telescope.load_extension("projects")
+
+    -- NOTE: Not the best approach, tried to use autocmd as well,
+    -- but had basically same bad results. For now sticking to this way.
+    if vim.v.argv[3] == nil then
+      vim.schedule(function()
+        local recent = require("project_nvim.utils.history").get_recent_projects()
+        telescope.extensions.projects.projects(recent)
+      end)
+    end
   end,
 }
