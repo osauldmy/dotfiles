a9e8b97798ec8207467857790da43bee5d8c815a
Dmytro Osaulenko <osauldmy@gmail.com>
2025-04-20 13:16:17 +0200

    nvim: ignore semantic highlighting (basedpyright vs treesitter)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -35,6 +35,13 @@ capabilities.textDocument.foldingRange = {
   lineFoldingOnly = true,
 }
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
+
 local setup_lsps = function()
   local lspconfig = require("lspconfig")
 
