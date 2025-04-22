ea525035a1e579af510fff42ea39515b667b0f22
Dmytro Osaulenko <osauldmy@gmail.com>
2025-04-22 09:21:59 +0200

    nvim: fix transparent background

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,12 @@
-local make_transparent_background = function()
-  vim.cmd([[
-  highlight Normal ctermbg=none guibg=none
-  highlight NonText ctermbg=none guibg=none
-  ]])
-end
-
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    init = function()
+    lazy = false,
+    config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
       vim.cmd([[colorscheme gruvbox]])
-      make_transparent_background()
     end,
   },
 }
