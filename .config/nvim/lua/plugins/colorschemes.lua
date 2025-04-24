aa3d6bd5edcdf571f6510d4798bc07f241fcef20
Dmytro Osaulenko <osauldmy@gmail.com>
2025-04-24 10:02:46 +0200

    nvim: don't show default colorschemes in picker

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,3 +1,34 @@
+-- Don't show default themes in picker
+vim.opt.wildignore:append({
+  "blue.vim",
+  "darkblue.vim",
+  "default.vim",
+  "delek.vim",
+  "desert.vim",
+  "elflord.vim",
+  "evening.vim",
+  "industry.vim",
+  "habamax.vim",
+  "koehler.vim",
+  "lunaperche.vim",
+  "morning.vim",
+  "murphy.vim",
+  "pablo.vim",
+  "peachpuff.vim",
+  "quiet.vim",
+  "ron.vim",
+  "shine.vim",
+  "slate.vim",
+  "sorbet.vim",
+  "retrobox.vim",
+  "torte.vim",
+  "unokai.vim",
+  "vim.lua",
+  "wildcharm.vim",
+  "zaibatsu.vim",
+  "zellner.vim",
+})
+
 return {
   {
     "ellisonleao/gruvbox.nvim",
