fed18e126d6330c144619eab5528b89ffbb0327d
Dmytro Osaulenko <osauldmy@gmail.com>
2025-04-29 21:04:45 +0200

    nvim: little style commit to prevent big diff on feature commit

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  priority = 100,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      ensure_installed = {
-        "vim",
-        "lua",
-        "regex",
-        -- NOTE: `comment` is commented out for now:
-        --       significantly slows down nvim with long comments in the code
-        -- "comment",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "python",
-      },
-      sync_install = false,
-      disable = {},
-      additional_vim_regex_highlighting = false,
-      highlight = { enable = true },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    priority = 100,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        ensure_installed = {
+          "vim",
+          "lua",
+          "regex",
+          -- NOTE: `comment` is commented out for now:
+          --       significantly slows down nvim with long comments in the code
+          -- "comment",
+          "bash",
+          "markdown",
+          "markdown_inline",
+          "python",
+        },
+        sync_install = false,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+        highlight = { enable = true },
+      })
+    end,
+  },
 }
