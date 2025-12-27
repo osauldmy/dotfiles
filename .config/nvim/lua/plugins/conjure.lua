6c434a5795dd79bd49cb4419a3238540ef760385
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-27 12:44:01 +0100

    nvim: full-width conjure repl window

--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -2,6 +2,7 @@ return {
   "Olical/conjure",
   ft = { "clojure", "lisp" },
   config = function()
+    vim.g["conjure#log#hud#width"] = 1
     vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
   end,
 }
